c16848da4ea8c9744031dae0d14109b5531cb732
richirrim <ricardoortega.dev@gmail.com>
2022-03-18 12:38:10 -0600

    Refactorizaa nombres de funciones y código feo.

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-/**
- * getPokemonLimit()
- * 
- * @param {String} id
- * @param {String} message
- */
-const renderMessage = function (id, message) {
-    const containerEl = document.getElementById(id)
-    const spanEl = document.createElement('span')
-    if (!containerEl) return
-
-    containerEl.innerHTML = ''
-    spanEl.setAttribute('class', 'showMessageError')
-    spanEl.textContent = message
-    containerEl.append(spanEl)
-}
-
-
-/**
- * getPokemonLimit()
- * 
- * Obtiene un número limitado de Pokemon.
- */
-const getPokemonLimit = async function () {
-    const LIMIT = 20
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}`)
-    let result = await response.json()
-    let pokemonList = result.results
-    let pokemonListId = []
-    
-    if (!pokemonList) return
-    
-    for (let pokemon of pokemonList) {
-        let id = pokemon.url.split('/').slice(-2)[0]
-        pokemonListId.push(id)
-    }
-    return pokemonListId
-
-}
+// La api solo tiene 898 pokemon.
+const TOTAL_AMOUNT_POKEMON = 898
 
 
 /**
@@ -45,18 +8,16 @@ const getPokemonLimit = async function () {
  * @param {Number} id
  *  Obtiene un unico elemento.
  */
- const getPokemon = async function (id) {
+ const fetchPokemon = async function (id) {
     if (!id) return
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-
     if (!response.ok) {
         renderMessage(
             'js-pokemonContainer', 
             'No se encontro el pokemon que estabas buscando😢.'
         )
         return
-    }
-
+    } 
     const result = await response.json()
     return result
 }
@@ -69,11 +30,11 @@ const getPokemonLimit = async function () {
  * devuelto por la función getPokemonLimit()
  */
 const getPokemonList = async function () {
+    const QUANTITY_POKEMON = 15
     let pokemonList = []
-    let pokemonListLimit = await getPokemonLimit()
     
-    for (let id of pokemonListLimit) {
-        const result = await getPokemon(id)
+    for (let i = 1; i <= QUANTITY_POKEMON; i++) {
+        const result = await fetchPokemon(i)
         pokemonList.push(result)
     }
     return pokemonList
@@ -88,19 +49,21 @@ const getPokemonList = async function () {
  * @param {Object} imageSprties Recibe un objeto con todos los sprites.
  * @param {Object} types
  */
- const createTemplateCard = function ({id, imageType = 'svg', imageSprites, name, types}) {
-    let image
-    if (imageType === 'svg') {
-        // Apartir del 650 no hay imagenes svg del pokemon.
-        // Por ende, devuelve svg si existe y, si no, la version 'png'.
-        image = imageSprites.other.dream_world.front_default ? imageSprites.other.dream_world.front_default : imageSprites.front_default
-    }
-    else image = imageSprites.front_default
-
-    const typeOne = types[0].type.name
-    const typeTwo = types.length === 2 
-        ? `<span class="tag">${types[1].type.name}</span>` 
-        : ''
+ const createTemplateCard = function ({
+    id, 
+    imageType = 'svg', 
+    imageSprites: {
+        front_default: imagePNG,
+        other: {dream_world: {front_default: imageSVG}}
+    }, 
+    name, 
+    types: [firstType, secondType]}) {
+     // Apartir del 650 no hay imagenes svg del pokemon.
+     // Por ende, devuelve svg si existe y, si no, la version 'png'.
+    // let [firstType1, secondType2] = types
+    let image = imageSVG || imagePNG
+    firstType = firstType.type.name
+    secondType = secondType ? `<span class="tag">${secondType.type.name}</span>` : ''
 
     let template = `
         <div class="card-pokemon__content">
@@ -111,12 +74,69 @@ const getPokemonList = async function () {
             <h2 class="card-pokemon__name">${name}</h2>
         </div>
         <div class="card-pokemon__tags">
-            <span class="tag">${typeOne}</span>
-            ${typeTwo}
+            <span class="tag">${firstType}</span>
+            ${secondType}
         </div>
     `
     return template
 }
+
+
+/**
+ * randomPokemon()
+ *  
+ */
+ const randomPokemon = async function () {
+    const id = Math.ceil(Math.random()*TOTAL_AMOUNT_POKEMON)
+    const pokemon = await fetchPokemon(id)
+    renderPokemon(pokemon)
+}
+
+
+/**
+ * getDataForm()
+ *  
+ */
+ const getDataForm = function () {
+    const formEl = document.getElementById('js-formSearch')
+    if (!formEl) return;
+    
+    formEl.addEventListener('submit', async function(e) {
+        e.preventDefault()
+        let pokemon
+        const idOrName = document.getElementById('js-inputSearch').value
+        if (!idOrName) return
+
+        pokemon = await fetchPokemon(idOrName)
+        if (!pokemon) {
+            formEl.reset()
+            return
+        }
+
+        idOrName.toLowerCase().trim()
+        renderPokemon(pokemon)
+        formEl.reset()
+    })
+}
+
+
+/**
+ * getPokemonLimit()
+ * 
+ * @param {String} id
+ * @param {String} message
+ */
+ const renderMessage = function (id, message) {
+    const containerEl = document.getElementById(id)
+    const spanEl = document.createElement('span')
+    if (!containerEl) return
+
+    containerEl.innerHTML = ''
+    spanEl.setAttribute('class', 'showMessageError')
+    spanEl.textContent = message
+    containerEl.append(spanEl)
+}
+
 
 
 /**
@@ -125,6 +145,8 @@ const getPokemonList = async function () {
  * Pinta cada compoonente card con info del 
  * pokemon en el layout.
  */
+
+
 const renderPokemonList = async function () {
     const pokemonListFragment = document.createDocumentFragment()
     const pokemonListEl = document.querySelector('#js-pokemon-list .grid')
@@ -175,41 +197,21 @@ const renderPokemon = async function (pokemon) {
 
 
 /**
- * getDataForm()
+ * renderRandomPokemon()
  *  
  */
-const getDataForm = function () {
-    const formEl = document.getElementById('js-formSearch')
-    if (!formEl) return;
-    
-    formEl.addEventListener('submit', async function(e) {
+const renderRandomPokemon = function () {
+    const linkEl = document.getElementById('js-linkRandomGenerator')
+    if (!linkEl) return
+
+    linkEl.addEventListener('click', function (e) {
         e.preventDefault()
-        const idOrName = document.getElementById('js-inputSearch').value.toLowerCase().trim()
-        const pokemon = await getPokemon(idOrName)
-
-        if (!pokemon && idOrName) {
-            formEl.reset()
-            return
-        }
-
-        renderPokemon(pokemon)
-        formEl.reset()
+        randomPokemon()
     })
 }
 
 
-/**
- * renderRandomPokemon()
- *  
- */
-const renderRandomPokemon = async function () {
-    const id = Math.ceil(Math.random()*898)
-    const pokemon = await getPokemon(id)
-    renderPokemon(pokemon)
-}
-
-
-// nota: la api solo tiene 898 pokemon.
 getDataForm()
+renderRandomPokemon()
 document.addEventListener('DOMContentLoaded', renderPokemonList)
-document.addEventListener('DOMContentLoaded', renderRandomPokemon)
+document.addEventListener('DOMContentLoaded', randomPokemon)
